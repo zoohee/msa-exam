8dcff27b5a1725e58b63e9b4999d4b87ea3de38e
zoohee <jjhh1545@gmail.com>
2024-12-01 01:23:34 +0900

    [product] feat: response header에 server port 추가

--- product/src/main/java/com/sparta/msa_exam/product/ProductController.java
+++ product/src/main/java/com/sparta/msa_exam/product/ProductController.java
@@ -4,11 +4,13 @@ import com.sparta.msa_exam.product.dto.request.AddProductRequest;
 import com.sparta.msa_exam.product.dto.response.AddProductResponse;
 import com.sparta.msa_exam.product.dto.response.GetProductResponse;
 import com.sparta.msa_exam.product.dto.response.GetProductsResponse;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.ws.rs.Path;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,6 +26,11 @@ public class ProductController {
     private String port;
 
     private final ProductService productService;
+
+    @ModelAttribute
+    public void setResponseHeader(HttpServletResponse response) {
+        response.setHeader("Server-Port", port);
+    }
 
     @PostMapping
     public ResponseEntity<AddProductResponse> addProduct(@RequestBody AddProductRequest request) {
